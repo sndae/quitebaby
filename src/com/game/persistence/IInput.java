6e35ba0d10aad1fb5087535b293523bd0662788f
knissel <knissel@localhost>
2012-10-01 17:10:39 -0400

    Added method declarations for interface IInput.

--- src/com/game/persistence/IInput.java
+++ src/com/game/persistence/IInput.java
@@ -7,5 +7,34 @@ package com.game.persistence;
  *
  */
 public interface IInput {
-
+	/**
+	 * Get the X axis value of the device's accelerometer.
+	 * @return the X value of the accelerometer.0
+	 */
+	public float getAccelX();
+	
+	/**
+	 * Get the Y axis value of the device's accelerometer.
+	 * @return the Y value of the accelerometer.
+	 */
+	public float getAccelY();
+	
+	
+	/**
+	 * Get the Z  axis value of the device's accelerometer.
+	 * @return the Z value of the accelerometer
+	 */
+	public float getAccelZ();
+	
+	/**
+	 * Get the X axis value of where the touch screen is being touched.
+	 * @return the X value of the touch screen.
+	 */
+	public float getTouchX();
+	
+	/**
+	 * Get the Y axis value of where the touch screen is being touched.
+	 * @return the Y value of the touch screen.
+	 */
+	public float getTouchY();
 }
