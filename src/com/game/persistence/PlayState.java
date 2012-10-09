37911c37cd8c5510704fa0b579aef9cfc25135d6
grote53 <grote53@localhost>
2012-10-09 17:35:14 -0400

    Added to the update() and render() methods of all four game state classes: update() retrieves input listeners from the game container, and render() retrieves rendering mechanisms from the game container.

--- src/com/game/persistence/PlayState.java
+++ src/com/game/persistence/PlayState.java
@@ -8,21 +8,22 @@ package com.game.persistence;
  */
 public class PlayState extends BaseGameState{
 	/**
-	 * Update game logic. Input can be retrieved from the game container. 
+	 * Update game logic. 
 	 * @param container the game container.
 	 */
 	@Override
 	public void update(IGameContainer container){
-		
+		//Get input listeners from the game container.
+		IInput input = container.getInput();
 	}
 	
 	/**
-	 * Render the updated game environment to the screen. Rendering mechanisms can 
-	 * be retrieved from the game container.
+	 * Render the updated game environment to the screen.
 	 * @param container the game container.
 	 */
 	@Override
 	public void render(IGameContainer container){
-		
+		//Get rendering mechanisms from the game container.
+		IGraphics graphics = container.getGraphics();
 	}
 }
