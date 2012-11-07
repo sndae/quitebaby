786c454c8208b0a237529dc60ce856d0e2eb794b
lee2db <lee2db@mail.uc.edu>
2012-11-07 17:44:14 -0500

    daniel

--- src/com/game/ui/MainGameActivity.java
+++ src/com/game/ui/MainGameActivity.java
@@ -10,23 +10,24 @@ import android.widget.Button;
 
 public class MainGameActivity extends Activity {
 
-Button new_game, resume_game, high_scores, settings, how_to_play,test_button;
-
-
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		Button new_game, resume_game, high_scores,options, how_to_play,test_button,quit;
 		new_game = (Button) findViewById(R.id.newGameButton);
 		resume_game = (Button) findViewById(R.id.resumeGameButton);
 		high_scores = (Button) findViewById(R.id.highScoresButton);
 		how_to_play = (Button) findViewById(R.id.howToPlayButton);
-		settings = (Button) findViewById(R.id.settingsButton);
+		options = (Button) findViewById(R.id.optionsButton);
+		quit = (Button) findViewById(R.id.quitButton);
 				
 
 		new_game.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
+				Intent intent = new 
 			}
 		});
 		resume_game.setOnClickListener(new View.OnClickListener() {
