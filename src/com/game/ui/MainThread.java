666c2bad64bab0d1579a8dcc9db6b12594d7460d
lee2db <lee2db@mail.uc.edu>
2012-12-01 15:38:51 -0500

    babyHappy

--- src/com/game/ui/MainThread.java
+++ src/com/game/ui/MainThread.java
@@ -1,16 +1,19 @@
 package com.game.ui;
 
+
+
+import android.graphics.Canvas;
 import android.view.SurfaceHolder;
 
 public class MainThread extends Thread{
 private SurfaceHolder surfaceHolder;
-private GameActivity gameActivity;
+private com.game.ui.Picture gameActivity;
 private boolean running;
 
-public MainThread(SurfaceHolder surfaceHolder, GameActivity gameActivity){
+public MainThread(SurfaceHolder surfaceHolder, com.game.ui.Picture picture){
 	super();
 	this.surfaceHolder = surfaceHolder;
-	this.gameActivity = gameActivity;
+	this.gameActivity = picture;
 }
 
 public void setRunning(boolean running){
@@ -18,7 +21,19 @@ public void setRunning(boolean running){
 }
 @Override
 public void run(){
+	Canvas canvas;
 	while(running){
+		canvas = null;
+	try {
+		canvas = this.surfaceHolder.lockCanvas();
+		synchronized(surfaceHolder){
+			this.gameActivity.onDraw(canvas);
+		}
+	} finally {
+		if(canvas != null){
+			surfaceHolder.unlockCanvasAndPost(canvas);
+		}
+	}
 		//update game state
 		//render state to the screen
 				  }
