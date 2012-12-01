811a7f5e5124502dcd99a9056a1308270060deb9
lee2db <lee2db@mail.uc.edu>
2012-12-01 15:38:51 -0500

    babyHappy

--- src/com/game/ui/Picture.java
+++ src/com/game/ui/Picture.java
@@ -8,33 +8,47 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class Picture extends SurfaceView implements SurfaceHolder.Callback{
-	public Picture(Context context){
-		super(context);
-	//intercept events
-	getHolder().addCallback(this);
-	setFocusable(true);
-	}
-	//@Override
-	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-	}
-	//@Override
-    public void surfaceCreated(SurfaceHolder holder) {
-    }
-	//@Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-		
-    } 
-	@Override
-	protected void onDraw(Canvas canvas){
-		canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.baby_happy), 10, 10, null);
-	}
+	public boolean babyHappy;
+	private MainThread thread;
+		public Picture(Context context){
+			super(context);
+		//intercept events
+		getHolder().addCallback(this);
+		//create thread
+		thread = new MainThread(getHolder(),this);
+		setFocusable(true);
+		}
+		//@Override
+		public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+		}
+		//@Override
+	    public void surfaceCreated(SurfaceHolder holder) {
+	    thread.setRunning(true);
+	    thread.start();
+	    }
+		//@Override
+	    public void surfaceDestroyed(SurfaceHolder holder) {
+		boolean retry = true;
+		while(retry){
+		try{
+			thread.join();
+			retry = false;
+		} catch(InterruptedException e)
+		{
+			
+		}
+		}
+	    } 
+		@Override
+		public void onDraw(Canvas canvas){
+			canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.baby_happy), 10, 10, null);
+		}
+}	
     
     
     
     
-    
-    
- }
+ 
 	
 	
 	
