b656924cc4ed513eb97ac37a70ee3c7cd62f2b92
grote53 <grote53@localhost>
2012-12-03 15:15:13 -0500

    Added thread killer to Picture

--- src/com/game/ui/Picture.java
+++ src/com/game/ui/Picture.java
@@ -7,60 +7,59 @@ import android.content.Context;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-public class Picture extends SurfaceView implements SurfaceHolder.Callback{
+public class Picture extends SurfaceView implements SurfaceHolder.Callback {
+	private static final String TAG = Picture.class.getSimpleName();
 	public boolean babyHappy;
 	private PlayState thread;
-		public Picture(Context context){
-			super(context);
-		//intercept events
+
+	public Picture(Context context) {
+		super(context);
+		// intercept events
 		getHolder().addCallback(this);
-		//create thread
+		// create thread
 		thread = new PlayState(this, getHolder(), new SensorListener(context));
 		setFocusable(true);
 		this.babyHappy = false;
-		}
-		//@Override
-		public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-		}
-		//@Override
-	    public void surfaceCreated(SurfaceHolder holder) {
-	    thread.setRunning(true);
-	    thread.start();
-	    }
-		//@Override
-	    public void surfaceDestroyed(SurfaceHolder holder) {
+	}
+
+	// @Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+	}
+
+	// @Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		thread.setRunning(true);
+		thread.start();
+	}
+
+	// @Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
 		boolean retry = true;
-		while(retry){
-		try{
-			thread.join();
-			retry = false;
-		} catch(InterruptedException e)
-		{
-			
-		}
-		}
-	    } 
-		@Override
-		public void onDraw(Canvas canvas){
-			
-			if(babyHappy){
-				canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.baby_happy), 10, 10, null);
-			}
-			else {
-				canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.baby_unhappy), 10, 10, null);
+		while (retry) {
+			try {
+				thread.setRunning(false);
+				thread.join();
+				retry = false;
+			} catch (InterruptedException e) {
+				Log.d(TAG, e.getMessage());
 			}
 		}
-}	
-    
-    
-    
-    
- 
-	
-	
-	
+	}
 
+	@Override
+	public void onDraw(Canvas canvas) {
 
+		if (babyHappy) {
+			canvas.drawBitmap(BitmapFactory.decodeResource(getResources(),
+					R.drawable.baby_happy), 10, 10, null);
+		} else {
+			canvas.drawBitmap(BitmapFactory.decodeResource(getResources(),
+					R.drawable.baby_unhappy), 10, 10, null);
+		}
+	}
+}
