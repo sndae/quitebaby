fb06ce5edb5a6c339ede9f32c026c58da64eaa39
knissel <knissel@localhost>
2012-12-03 16:24:17 -0500

    Added JavaDoc

--- src/com/game/integration/Sound.java
+++ src/com/game/integration/Sound.java
@@ -6,19 +6,32 @@ import android.content.Context;
 import android.media.MediaPlayer;
 
 public class Sound {
+	//initialize global media player value 
 	public MediaPlayer mp;
 
+	/**
+	 * Method to initialize the cry baby sound 
+	 * @param context
+	 */
 	public Sound(Context context) {
 		this.mp = MediaPlayer.create(context, R.raw.baby);
 	
 	}
 
+	/**
+	 * Method to play the crying baby sound 
+	 * @param context
+	 */
 	public void PlaySound(Context context) {
 		if (!this.mp.isPlaying()) {
 			
 			mp.start();
 		}
 	}
+	
+	/**
+	 * Method to pause the baby from crying. 
+	 */
 
 	public void StopSound() {
 		if (this.mp.isPlaying()) {
@@ -29,6 +42,4 @@ public class Sound {
 		}
 	}
 
-	public void PauseSound() {
-	}
 }
